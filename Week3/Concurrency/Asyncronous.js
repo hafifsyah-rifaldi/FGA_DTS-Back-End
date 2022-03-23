f769eee508613b44ec86369a07d723e8d968c26f
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-23 19:47:15 +0700

    Added syntax Callback and Promise

--- Week3/Concurrency/Asyncronous.js
+++ Week3/Concurrency/Asyncronous.js
@@ -1,18 +1,46 @@
-// ! Contoh Callback Function (berurutan karena asyncronous hanya 4 task)
-
-setTimeout(() => {
-    console.log("Bangun tidur kuterus mandi");
-    setTimeout(() => {
-        console.log("Tidak lupa menggosok gigi");
+// ! Contoh Callback Function, Promise (asyncronous hanya 4 task)
+const Task = (taskDetail,duration) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log("Habis mandi kutolong ibu");
-            setTimeout(() => {
-                console.log("Membersihkan tempat tidurku");
+            if (duration > 3000) {
+                reject("Enakan ngopi dulu.. gak ngopi gak uuu...")
+            }else{
+                console.log(taskDetail);
+                resolve();
+            }
+        }, duration);
+    });
+};
+
+Task("Bangun tidur kuterus mandi",3000)
+    .then(() => Task("Tidak lupa menggosok gigi",2000))
+    .then(() => Task("Habis mandi kutolong ibu",1000))
+    .then(() => Task("Membersihkan tempat tidurku",4000))
+    .catch(message => console.log(message));
+
+    
+// Task("Bangun tidur kuterus mandi",3000,() => {
+//     Task("Tidak lupa menggosok gigi", 2000, () => {
+//         Task("Habis mandi kutolong ibu",1000, () => {
+//             Task("Membersihkan tempat tidurku",4000,() => {});
+//         })
+//     })
+// });
+
+
+// setTimeout(() => {
+//     console.log("Bangun tidur kuterus mandi");
+//     setTimeout(() => {
+//         console.log("Tidak lupa menggosok gigi");
+//         setTimeout(() => {
+//             console.log("Habis mandi kutolong ibu");
+//             setTimeout(() => {
+//                 console.log("Membersihkan tempat tidurku");
                 
-            }, 4000);
-        }, 1000);
-    }, 2000);
-}, 3000);
+//             }, 4000);
+//         }, 1000);
+//     }, 2000);
+// }, 3000);
 
 
 
